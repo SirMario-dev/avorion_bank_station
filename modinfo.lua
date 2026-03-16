eceeafa2e79b4f64b9d94fa8d00d0cb83e5ae8c8
SirMario <Lancer2013@ail.com>
2026-03-16 14:23:28 -0400

    Updating Mod Authors

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ meta =
     title = "Bank Station | [v.1.3]",
     type = "mod",
     description = "Adds a new Bank station type. This station allows for the users to deposit money, which earns interest but cannot be used until it is withdrawn. Deposited money earns 2% compounding interest every 5 minutes.",
-    authors = {"Sir Mario"},
+    authors = {"Sir Mario", "Kxawzitan"},
     version = "1.3",
     dependencies = {
         {id = "Avorion", min = "2.3", max = "2.*"}
